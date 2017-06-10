940b29ff8b0a2163c2e21ea2887e0752a3b44427
A Arrow <oneone@gmail.com>
2017-06-10 01:22:54 +0000

    channel

---
+++ chat4work/Channel.swift
@@ -0,0 +1,43 @@
+import Foundation
+import ObjectMapper
+
+struct Channels: Mappable {
+  
+  var results: Array<Card>?
+  
+  init?(map: Map) {
+  }
+  
+  mutating func mapping(map: Map) {
+    results <- map["channels"]
+  }
+}
+
+struct Channel: Mappable, Equatable {
+  
+  var id: String?
+  var name: String?
+  var creator: String?
+  var num_members: Int?
+  var created: Int64?
+  
+  init?(map: Map) {
+  }
+  
+  mutating func mapping(map: Map) {
+    id <- map["id"]
+    name <- map["name"]
+    creator <- map["creator"]
+    num_members <- map["num_members"]
+    created <- map["created"]
+  }
+  
+  func briefDescription() -> String {
+    return (name? "")
+  }
+  
+  static func ==(lhs: Card, rhs: Card) -> Bool {
+    return lhs.id == rhs.id
+  }
+  
+}

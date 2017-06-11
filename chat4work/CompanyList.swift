dda5a78dd35a98b32bab90657d463e88cb44ed80
A Arrow <oneone@gmail.com>
2017-06-11 19:40:47 +0000

    red

--- chat4work/CompanyList.swift
+++ chat4work/CompanyList.swift
@@ -21,6 +21,15 @@ class CompanyWithRed: NSView {
   
   func checkRedDotStatus(notification: NSNotification) {
     
+    let json = notification.object as! [String: Any]
+    NSLog("\(json)")
+    //2017-06-11 03:53:46.014074+0000 boring-company-chat[7958:82613] ["team": T035N23CL, "source_team": T035N23CL, "user": U035LF6C1, "text": wefwef, "channel": D1KD59XH9, "type": message, "ts": 1497153225.487018]
+    
+    if let team = json["team"] {
+    if (team as! String) == self.team_id {
+      red.isHidden = false
+    }
+    }
   }
 
   override init(frame frameRect: NSRect) {

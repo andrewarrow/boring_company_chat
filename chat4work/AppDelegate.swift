91a00c70de18f7f88686e228a1bd970d980f61d0
A Arrow <oneone@gmail.com>
2017-06-24 08:20:27 +0000

    text

--- chat4work/AppDelegate.swift
+++ chat4work/AppDelegate.swift
@@ -64,26 +64,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
   }
   
-  @IBAction func listChannelsWithCount(_ sender:NSObject) {
-    NSLog("Wefwfe")
-  }
   
+  /*
   @IBAction func sortByLastMsgDate(_ sender:NSObject) {
-    
-    /*
-    win = NSWindow(contentRect: NSMakeRect(100, 100, 600, 200),
-                       styleMask: NSResizableWindowMask,
-                       backing: NSBackingStoreType.buffered, defer: true)
-    win.addButton(withTitle: "Yes")
-    
-    let nswc = NSWindowController(window: win)
-    //win?.makeKeyAndOrderFront(self)
-    
-    NSApplication.shared().keyWindow?.beginSheet(nswc.window!, completionHandler: { [unowned self] (returnCode) -> Void in
-        if returnCode == NSAlertFirstButtonReturn {
-            //self.dataModel.removeAll()
-        }
-    })*/
     
     self.alert = NSAlert()
     alert?.messageText = "S O R T I N G"
@@ -100,7 +83,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     NotificationCenter.default.post(
         name:NSNotification.Name(rawValue: "sortByLastMsgDate"),
         object: alert)
-  }
+  } */
     
   @IBAction func pref(_ sender:NSObject) {
  
